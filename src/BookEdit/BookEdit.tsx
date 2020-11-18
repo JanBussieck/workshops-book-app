db1a93be0a755efab32e6dd45e8baa58c259962b
Jan Bussieck <jan.bussieck@gmail.com>
2020-11-18 10:10:00 +0100

    Set up for custom form validation

--- src/BookEdit/BookEdit.tsx
+++ src/BookEdit/BookEdit.tsx
@@ -10,9 +10,14 @@ const BookEdit: React.FC<BookEditProps> = ({book, onSubmit}) => {
   // useState hooks
   // set initial values for book form title / isbn
   const [form, setForm] = useState(book);
+  const [errors, setErrors] = useState({
+    title: null,
+    isbn: null
+  });
 
   const handleFormInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { target: { value, name } } = event;
+
     setForm((prevForm) => ({
       ...prevForm,
       [name]: value
@@ -28,19 +33,21 @@ const BookEdit: React.FC<BookEditProps> = ({book, onSubmit}) => {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title">Title: </label>
+      {errors.title && <div style={{border: '2px solid red'}}>errors.title</div>}
       <input id="title"
         name="title"
         type="text"
-        required
         value={form.title}
+        onBlur={() => console.log("I was blurred")}
         onChange={handleFormInputChange} />
       <br />
       <label htmlFor="isbn">ISBN: </label>
+      {errors.isbn && <div style={{border: '2px solid red'}}>errors.isbn</div>}
       <input id="isbn"
         name="isbn"
         type="text"
-        required
         value={form.isbn}
+        onBlur={() => console.log("I was blurred")}
         onChange={handleFormInputChange} />
       <br />
       <button type='submit'>Submit</button>
