255f2bb15e7e152b5c4f21d629cf8577da6d07ec
Jan Bussieck <jan.bussieck@gmail.com>
2020-11-18 09:21:28 +0100

    Lift up book state from BookList to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BookList from './BookList/BookList';
-import logo from './logo.svg';
+import type {Book} from './BookItem/BookItem';
+import BookEdit from './BookEdit/BookEdit';
 
 function App() {
-  return (
+  const [books, setBooks] = useState<Array<Book>>([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:4730/books').then((response) => {
+      return response.json();
+    }).then((books) => {
+      setBooks(books);
+    }).catch((error) => {
+      console.warn(error);
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  }, [setBooks, setIsLoading])
+
+  const book = books[0];
+
+  return isLoading ? (
+    <div>... Loading books ...</div>
+  ) : (
     <div>
-      <BookList />
+      <BookList books={books} />
+      {book && <BookEdit book={book} />}
     </div>
   );
 }
--- src/BookList/BookList.tsx
+++ src/BookList/BookList.tsx
@@ -1,39 +1,17 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import BookItem from '../BookItem/BookItem';
-import BookEdit from '../BookEdit/BookEdit';
 import type {Book} from '../BookItem/BookItem';
 
-const BookList: React.FC = () => {
+const BookList: React.FC<{ books: Array<Book> }> = ({books}) => {
 
-  const [books, setBooks] = useState<Array<Book>>([])
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('http://localhost:4730/books').then((response) => {
-      return response.json();
-    }).then((books) => {
-      setBooks(books);
-    }).catch((error) => {
-      console.warn(error);
-    }).finally(() => {
-      setIsLoading(false);
-    });
-  }, [setBooks, setIsLoading])
-
-  const book = books[0];
   return (
     <Fragment>
       <h1>List of Books</h1>
-      {isLoading ? (
-        <div>... Loading books ...</div>
-      ) : (
-        <ul>
-          {books.map((book) => {
-            return <BookItem key={book.isbn} book={book} />
-          })}
-        </ul>
-      )}
-      {book && <BookEdit book={book} />}
+      <ul>
+        {books.map((book) => {
+          return <BookItem key={book.isbn} book={book} />
+        })}
+      </ul>
     </Fragment>
   );
 }
